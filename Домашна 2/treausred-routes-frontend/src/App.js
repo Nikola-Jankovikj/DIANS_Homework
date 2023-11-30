fa0ccf2536557cffb3a97bdba8fbe0f6aaf162b1
Nikola Jankovikj <nikola.jankovikj@students.finki.ukim.mk>
2023-11-30 17:44:41 +0100

    Technical-Prototype added login and register pages and refactored code

--- Домашна 2/treausred-routes-frontend/src/App.js
+++ Домашна 2/treausred-routes-frontend/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./Home";
-import Monasteries from "./Monasteries";
-import Archaeological from "./Archaeological";
-import Museums from "./Museums";
+import Login from "./Login";
+import Register from "./Register";
+
 
 function App() {
 
@@ -12,9 +12,8 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/home" Component={Home} />
-                <Route path="/monasteries" Component={Monasteries} />
-                <Route path="/archaeological" Component={Archaeological} />
-                <Route path="/museums" Component={Museums}/>
+                <Route path="/login" Component={Login} />
+                <Route path="/register" Component={Register} />
             </Routes>
         </Router>
     );
--- Домашна 2/treausred-routes-frontend/src/Home.js
+++ Домашна 2/treausred-routes-frontend/src/Home.js
@@ -1,11 +1,69 @@
+import {MapContainer, Marker, TileLayer, Popup} from "react-leaflet";
 import "leaflet/dist/leaflet.css"
+import {useEffect, useState} from "react";
+import {Icon} from "leaflet/dist/leaflet-src.esm";
+import MarkerClusterGroup from "react-leaflet-cluster";
 import './MapComponent.css';
-import MapComponent from "./MapComponent";
 
 const Home = () => {
 
+    const [url, setUrl] = useState('http://localhost:8080/api/all')
+    const [state, setState] = useState([]);
+
+    useEffect(() => {
+        console.log(url)
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setState(data);
+            })
+    }, [url]);
+
+    const customIcon = new Icon({
+        iconUrl: require('./resources/location-pin.png'),
+        iconSize: [38, 38]
+    })
+
+    const findAll = () => {
+        setUrl('http://localhost:8080/api/all')
+    }
+
+    const findMuseums = () => {
+        setUrl('http://localhost:8080/api/museums')
+    }
+
+    const findArchaeologicalSites = () => {
+        setUrl('http://localhost:8080/api/archaeologicalSites')
+    }
+
+    const findMonasteries = () => {
+        setUrl('http://localhost:8080/api/monasteries')
+    }
+
     return(
-        <MapComponent url={'http://localhost:8080/api/all'} />
+
+        <MapContainer center={[41.9981, 21.4254]} zoom={15}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <div id="nav">
+                <button onClick={findAll}>All</button>
+                <button onClick={findMuseums}>Museums</button>
+                <button onClick={findArchaeologicalSites}>Archaeological Sites</button>
+                <button onClick={findMonasteries}>Monasteries</button>
+            </div>
+
+            <MarkerClusterGroup chunkedLoading>
+                {state.map(obj =>
+                    <Marker key={obj.id} position={[obj.lat, obj.lon]} icon={customIcon}>
+                        <Popup><h2>{obj.name}</h2></Popup>
+                    </Marker>
+                )}
+            </MarkerClusterGroup>
+
+        </MapContainer>
     )
 }
 
