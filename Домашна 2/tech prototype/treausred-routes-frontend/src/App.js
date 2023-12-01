f1a4e597ebe1b8ebce1a239a32eaa97bfdf996dd
todorjovanovski <todor_pp@yahoo.com>
2023-12-01 16:13:34 +0100

    Added Styles and GUI Mockups

--- Домашна 2/tech prototype/treausred-routes-frontend/src/App.js
+++ Домашна 2/tech prototype/treausred-routes-frontend/src/App.js
@@ -4,7 +4,8 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
-import Profile from "./Profile";
+import Favorites from "./Favorites";
+import Profile from "./Profile"
 
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
     return(
         <Router>
             <Routes>
+                <Route path="" Component={Home} />
                 <Route path="/home" Component={Home} />
                 <Route path="/login" Component={Login} />
                 <Route path="/register" Component={Register} />
                 <Route path="/profile" Component={Profile} />
+                <Route path="/favorites" Component={Favorites} />
             </Routes>
         </Router>
     );
