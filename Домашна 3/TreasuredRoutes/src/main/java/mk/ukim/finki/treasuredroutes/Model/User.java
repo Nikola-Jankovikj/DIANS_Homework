11c309ab1fdde61939daa04ea6d20ce52e3f1751
todorjovanovski <todor_pp@yahoo.com>
2023-12-15 23:34:32 +0100

    Initial backend setup for favorites feature

---
+++ Домашна 3/TreasuredRoutes/src/main/java/mk/ukim/finki/treasuredroutes/Model/User.java
@@ -0,0 +1,26 @@
+package mk.ukim.finki.treasuredroutes.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "MAP_USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
