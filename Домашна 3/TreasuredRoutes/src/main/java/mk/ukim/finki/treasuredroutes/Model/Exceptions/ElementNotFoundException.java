c6998428c0429f5703fb83827fb8a970237c64a6
mateycardula <mateycardula@gmail.com>
2023-12-20 14:33:18 +0100

    Initial commit

--- Домашна 3/TreasuredRoutes/src/main/java/mk/ukim/finki/treasuredroutes/Model/Exceptions/ElementNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.treasuredroutes.Model.Exceptions;
-
-public class ElementNotFoundException extends RuntimeException{
-    public ElementNotFoundException(Long id) {
-        super(String.format("Element with id %d was not found", id));
-    }
-}
--- Домашна 3/TreasuredRoutes/src/main/java/mk/ukim/finki/treasuredroutes/Repository/ElementRepository.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.treasuredroutes.Repository;
-
-import mk.ukim.finki.treasuredroutes.Model.Element;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ElementRepository extends JpaRepository<Element, Long> {
-}
