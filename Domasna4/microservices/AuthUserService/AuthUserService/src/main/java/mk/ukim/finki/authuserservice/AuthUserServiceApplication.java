93fb18df324b5325948c76960eb84974aafdcb72
todorjovanovski <todor_pp@yahoo.com>
2024-01-04 05:20:17 +0100

    Initial eureka server setup

--- Domasna4/microservices/AuthUserService/AuthUserService/src/main/java/mk/ukim/finki/authuserservice/AuthUserServiceApplication.java
+++ Domasna4/microservices/AuthUserService/AuthUserService/src/main/java/mk/ukim/finki/authuserservice/AuthUserServiceApplication.java
@@ -2,8 +2,10 @@ package mk.ukim.finki.authuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthUserServiceApplication {
 
     public static void main(String[] args) {
